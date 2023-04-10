f71c2e813527baec4d7ac59ba5280361440a0ce0
Samuel Cho <chos5@nku.edu>
2023-04-09 22:46:34 -0400

    testhmac updated

--- security-examples/testhmac.js
+++ security-examples/testhmac.js
@@ -8,7 +8,7 @@ const { createHmac } = require('crypto');
 
 const password1 = 'hello!';
 const password2= 'hello! again';
-const message = 'hello wrold'
+const message = 'hello world'
 
 // We need to generate the same hash only when we know the password
 // Server can be sure the hmac hash is correct only when the sender has the correct password 
@@ -17,3 +17,22 @@ const hmac2 = createHmac('sha256', password2).update(message).digest('hex');
 
 console.log(hmac1)
 console.log(hmac2)
+
+// Receiver receives the message and the HMAC
+// Receiver can verify the message is not compromised by creating the HMAC again
+const hmac_created = createHmac('sha256', password1).update(message).digest('hex');
+if (hmac_created == hmac1) {
+    console.log(`${message} is not compromised. `)
+}
+else {
+    console.log(`${message} is compromised. `)
+}
+
+// message is compromised
+const hmac_created2 = createHmac('sha256', password2).update(message + '?').digest('hex');
+if (hmac_created2 == hmac2) {
+    console.log(`${message} is not compromised. `)
+}
+else {
+    console.log(`${message} is compromised. `)
+}
