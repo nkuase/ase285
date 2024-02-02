d79fed6365e5a0b108c50a714db9c394ca3fb9a2
Samuel Cho <chos5@nku.edu>
2024-02-02 12:24:16 -0500

    update for module/import

--- javascript/module/import/module/arithmetic.js
+++ javascript/module/import/module/arithmetic.js
@@ -9,5 +9,3 @@ export const mul = (a, b) => {
     }
     return result
 }
-  
-//export default mul;
--- javascript/module/import/module/arithmetic2.js
+++ javascript/module/import/module/arithmetic2.js
@@ -1,7 +1,3 @@
-export const add = (a, b) => {
-    return a + b;
-};
-export const sub = (a, b) => {
-    return a - b;
-};
+export const add = (a, b) => a + b;
+export const sub = (a, b) => a - b;
   
