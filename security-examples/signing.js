5283a518868a1b36f5febae8341593286894db41
Samuel Cho <chos5@nku.edu>
2024-02-23 17:04:59 -0500

    signature

--- security-examples/signing.js
+++ security-examples/signing.js
@@ -9,18 +9,18 @@ const signer = createSign('rsa-sha256');
 
 signer.update(data);
 
-const siguature = signer.sign(privateKey, 'hex');
-console.log(siguature);
+const signature = signer.sign(privateKey, 'hex');
+console.log(signature);
 
 /// VERIFY
 let verifier = createVerify('rsa-sha256');
 const compromised = data + '???'
 verifier.update(compromised);
-const isVerified1 = verifier.verify(publicKey, siguature, 'hex');
+const isVerified1 = verifier.verify(publicKey, signature, 'hex');
 console.log(isVerified1);
 
 verifier = createVerify('rsa-sha256');
 const non_compromised = data
 verifier.update(non_compromised);
-const isVerified2 = verifier.verify(publicKey, siguature, 'hex');
+const isVerified2 = verifier.verify(publicKey, signature, 'hex');
 console.log(isVerified2);
